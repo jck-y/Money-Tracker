c3ad1ec7730e77a16e12f541fa4ef5d91cae69fa
jck-y <s2200582@student.unklab.ac.id>
2024-03-22 06:19:08 +0800

    adding image picker in sign up

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,15 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import {Button, Gap, PageHeader, TextInput} from '../../components';
+import { Button, Gap, PageHeader, TextInput } from '../../components';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 const SignUp = () => {
   const navigation = useNavigation();
+  const [selectImage, setselectImage]=useState('')
+  const ImagePicker = () => {
+   let options = {
+    storageOptions:{
+      path: 'image',
+    },
+   };
+
+   launchImageLibrary(options, response =>{
+    setselectImage(response.assets[0].uri)
+    console.log(response.assets[0].uri)
+   })
+  };
+
   return (
     <View style={styles.container}>
-      <PageHeader label="Sign Up" backButton={true}navigation={navigation} nav="SignIn" />
+      <PageHeader label="Sign Up" backButton={true} navigation={navigation} nav="SignIn" />
       <Gap height={24} />
       <View style={styles.contentWrapper}>
-        <Gap height={152} />
+        <Gap height={26} />
+        <TouchableOpacity onPress={ImagePicker} style={styles.outerCircle}>
+          <Text style={styles.textphoto}>Add Photo </Text>
+        </TouchableOpacity>
+        <Gap height={16} />
         <TextInput
           label="Full Name"
           placeholder="Type your full name"
@@ -26,9 +45,7 @@ const SignUp = () => {
         />
         <Gap height={16} />
         <Button
-          label="Create New Account"
-          backgroundColor="#8D92A3"
-          textColor="#FFFFFF"
+          label="Continue"
           onPress={() => navigation.navigate('SignUp')}
         />
       </View>
@@ -36,16 +53,37 @@ const SignUp = () => {
   );
 };
 
-
-export default SignUp
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  contentWrapper:{
+  outerCircle: {
+    width: 110,
+    height: 110,
+    borderRadius: 100, 
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderStyle: 'dashed', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf:'center',
+  },
+  textphoto:{
+    display:'flex',
+    width:90,
+    height:90,
+    borderRadius:100,
+    backgroundColor:'#F0F0F0',
+    fontSize:21,
+    textAlign:'center',
+    textAlignVertical: 'center',
+    color: '#8D92A3',
+  },
+  contentWrapper: {
     backgroundColor: '#FFFFFF',
     flex: 1,
     paddingHorizontal: 24,
   },
-})
+});
+
+export default SignUp;
